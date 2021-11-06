2b7e79a50d5df16fa9eb7c00a9a295a11ac5a619
Abhushan <apradhan7678@gmail.com>
2021-11-06 17:11:28 -0500

    Heatmap Co-authored-by: sn51574 <sn51574@users.noreply.github.com> Co-authored-by: Sujan Niroula <sujan2003@users.noreply.github.com> Co-authored-by: dontcallmefooboy <dontcallmefooboy@users.noreply.github.com> Co-authored-by: drewhedrick <drewhedrick@users.noreply.github.com> Co-authored-by: DylanTopic <DylanTopic@users.noreply.github.com> Co-authored-by: Jmoney16 <Jmoney16@users.noreply.github.com> Co-authored-by: Tyler Emanuel <temanuel1@users.noreply.github.com>

---
+++ src/utils/stats.js
@@ -0,0 +1,87 @@
+export const stats = {    
+    northwestIowa: {
+        totalHomeless: 378,
+        womenHomeless: 132,
+        childrenHomeless: 18,
+    },
+    woodburyDakota: {
+        totalHomeless: 288,
+        womenHomeless: 134,
+        childrenHomeless: 15,
+    },
+    upperDesMoines: {
+        totalHomeless: 349,
+        womenHomeless: 131,
+        childrenHomeless: 22,
+    },
+    twoRivers: {
+        totalHomeless: 382,
+        womenHomeless: 147,
+        childrenHomeless: 23,
+    },
+    omahaCouncilBluffs: {
+        totalHomeless: 187,
+        womenHomeless: 62,
+        childrenHomeless: 17,
+    },
+    northCentralIowa: {
+        totalHomeless: 246,
+        womenHomeless: 93,
+        childrenHomeless: 26,
+    },
+    rollingHills: {
+        totalHomeless: 587,
+        womenHomeless: 236,
+        childrenHomeless: 45,
+    },
+    northEast: {
+        totalHomeless: 304,
+        womenHomeless: 136,
+        childrenHomeless: 32,
+    },
+    polk: {
+        totalHomeless: 681,
+        womenHomeless: 271,
+        childrenHomeless: 56,
+    },
+    easternIowa: {
+        totalHomeless: 365,
+        womenHomeless: 171,
+        childrenHomeless: 22,
+    },
+    balanceOfCounties: {
+        totalHomeless: 184,
+        womenHomeless: 83,
+        childrenHomeless: 13,
+    },
+    southCentralWest: {
+        totalHomeless: 356,
+        womenHomeless: 124,
+        childrenHomeless: 30,
+    },
+    johnsonWashington: {
+        totalHomeless: 350,
+        womenHomeless: 111,
+        childrenHomeless: 19,
+    },
+    southeastIowa: {
+        totalHomeless: 331,
+        womenHomeless: 120,
+        childrenHomeless: 20,
+    },
+    quadCitiesBiState: {
+        totalHomeless: 349,
+        womenHomeless: 148,
+        childrenHomeless: 23,
+    },
+    blackhawkGrundyTama: {
+        totalHomeless: 321,
+        womenHomeless: 125,
+        childrenHomeless: 18,
+    },
+    linnBentonJones: {
+        totalHomeless: 553,
+        womenHomeless: 194,
+        childrenHomeless: 54,
+    }
+}
